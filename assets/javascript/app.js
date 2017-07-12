85d9135bfb9ca106390ee7ab9bf7463c1ab34e40
cmoxey8719 <kirbymox@yahoo.com>
2017-07-11 22:02:50 -0400

    what i tried to do

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+//declaring variables
+
+var countdown = 0;
+var correct = 0;
+var wrong = 0;
+var unassigned = 0;
+
+
+// an object with all the questions, possible answers and correct answers
+
+
+//a function that dynamically makes and appends the questions to the DOM
+
+
+// a function that makes and appends the questions to the DOM that connects to the right question
+
+//onclick function that starts the timer, reveals the questions and hides the start button
+
+//onclick function that submits the values from the radio buttons into 3 variables(correct, wrong and unassigned), stops the timer and prints the results of each variable. also reveal the restart button
+
+//onclick function that resets the game without reloading the page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
